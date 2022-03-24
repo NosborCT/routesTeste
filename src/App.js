14d6883e2d502434e8f2a301ece04acbab7a3161
NosborCT <hainiken08@gmail.com>
2022-03-24 17:36:13 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Pagina1 from './componentes/Pagina1';
+import Pagina2 from './componentes/Pagina2';
+
+
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h2>Links</h2>
+
+        <ul>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/Pagina1"> Pagina 1 </Link>
+          </li>
+          <li>
+            <Link to="/Pagina2">Pagina 2</Link>
+          </li>
+
+        </ul>
+
+        
+        <Switch>
+          <Route exact path="/">
+            <home />
+          </Route>
+          <Route path="/Pagina1">
+            <Pagina1 />
+          </Route>
+          <Route path="/Pagina2">
+            <Pagina2 />
+          </Route>
+        </Switch>
+          
+
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default App();
+
---
+++ src/componentes/home.js
@@ -0,0 +1,11 @@
+function home(){
+    
+    <div>
+        <h1> home</h1>
+        <p> Conteudo da pagina</p>
+        
+    </div>
+
+}
+
+export default home
